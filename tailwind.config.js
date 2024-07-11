156aad837bd702714aa65d20c4cf45ba199a95be
JKodes <adrainford09@gmail.com>
2024-07-11 19:39:14 -0400

    fix tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        madimi: ['Madimi One', 'cursive'],
+        ruda: ['Ruda', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
